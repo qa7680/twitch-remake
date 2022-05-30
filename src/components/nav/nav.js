3eb4c6fd6cd01de4e0452320bc62e747875fd7de
qa7680 <qa.ali7680@gmail.com>
2022-05-29 20:00:54 -0400

    finished dropdowns

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,73 +1,115 @@
 import "..//nav/nav.scss";
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 const Nav =() => {
 
     const [ moreToggled, setMoreToggled ] = useState(false);
     const [ primeToggled, setPrimeToggled ] = useState(false);
     const [ profileToggled, setProfileToggled] = useState(false);
+    const moreNode = useRef();
+    const moreContent = useRef();
+    const primeNode = useRef();
+    const primeText = useRef();
+    const profileNode = useRef();
+    const profileContent = useRef();
 
-    const moreClicked = () => {
-        setMoreToggled(true);
-    };
-    const primeClicked = () => {
-        setPrimeToggled(true);
-    };
-    const profileClicked = () => {
-        setProfileToggled(true);
+    const hideMore = () => {
+        setMoreToggled(!moreToggled);
     }
 
-    const bodyClicked = () => {
-        if(moreToggled){
-            setMoreToggled(false);
-        }
-        if(primeToggled){
+    const outsideMore = (e) => {
+        if(moreNode.current?.contains(e.target) ||
+            moreContent.current?.contains(e.target)){
+                return;
+            }else{
+                setMoreToggled(false);
+            }
+    };
+
+    const outsidePrime = (e) => {
+        if(primeNode.current?.contains(e.target) ||
+        primeText.current?.contains(e.target)){
+            return;
+        }else{
             setPrimeToggled(false);
-        }
-        if(profileToggled){
-            setProfileToggled(false);
         }
     };
 
+    const outsideProfile = (e) => {
+        if(profileNode.current?.contains(e.target) ||
+        profileContent.current?.contains(e.target)){
+            return;
+        }else{
+            setProfileToggled(false);
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener('mousedown', outsideMore)
+
+        //cleanup
+        return() => {
+            document.removeEventListener('mousedown', outsideMore)
+        }
+    }, [moreToggled])
+
+    useEffect(() => {
+        document.addEventListener('mousedown', outsidePrime)
+
+        //cleanup
+        return() => {
+            document.removeEventListener('mousedown', outsidePrime)
+        }
+    }, [primeToggled])
+
+    useEffect(() => {
+        document.addEventListener('mousedown', outsideProfile)
+
+        //cleanup
+        return() => {
+            document.removeEventListener('mousedown', outsideProfile)
+        }
+    }, [profileToggled])
+
+
     return(
-        <body onClick={bodyClicked}>
             <nav className="navigation">
                 <div className="left">
                     <img alt="twitch icon" src={require("../../icons/navIcon3.png")} style = {{width:"32px", height: "32px"}}></img>
                     <button className="browse">Browse</button>
                     <div className="dropdown">
-                        <img onClick={moreClicked} alt="more options" className="moreVert" title="more" src={require("../../icons/verticalDots2.png")} />
-                        { moreToggled ?
-                        <div className="dropdown-content">
+                        <img onClick={hideMore} ref={moreNode} alt="more options" className="moreVert" title="more" src={require("../../icons/verticalDots2.png")} />
+                        { moreToggled &&
+                        <div ref={moreContent} className="dropdown-content">
                             <div className="general">
                                 <p>GENERAL</p>
-                                <a href = "#">About</a>
-                                <a href = "#">Advertisers</a>
-                                <a href = "#">Blog</a>
-                                <a href = "#">Developers</a>
-                                <a href = "#">Download Apps</a>
-                                <a href = "#">Gift Card</a>
-                                <a href = "#">IGDB</a>
-                                <a href = "#">Jobs</a>
-                                <a href = "#">Loot Cave-Merch Store</a>
-                                <a href = "#">Music on Twitch</a>
-                                <a href = "#">Partners</a>
-                                <a href = "#">Press</a>
-                                <a href = "#">Turbo</a>
+                                <a onClick={hideMore} href = "#">About</a>
+                                <a onClick={hideMore} href = "#">Advertisers</a>
+                                <a onClick={hideMore} href = "#">Blog</a>
+                                <a onClick={hideMore} href = "#">Developers</a>
+                                <a onClick={hideMore} href = "#">Download Apps</a>
+                                <a onClick={hideMore} href = "#">Gift Card</a>
+                                <a onClick={hideMore} href = "#">IGDB</a>
+                                <a onClick={hideMore} href = "#">Jobs</a>
+                                <a onClick={hideMore} href = "#">Loot Cave-Merch Store</a>
+                                <a onClick={hideMore} href = "#">Music on Twitch</a>
+                                <a onClick={hideMore} href = "#">Partners</a>
+                                <a onClick={hideMore} href = "#">Press</a>
+                                <a onClick={hideMore} href = "#">Turbo</a>
                             </div>
                             <div className="help">
                                 <p>HELP & LEGAL</p>
-                                <a href = "#">Accessibility Statement</a>
-                                <a href = "#">Ad Choices</a>
-                                <a href = "#">Community Guidelines</a>
-                                <a href = "#">Cookie Policy</a>
-                                <a href = "#">Help</a>
-                                <a href = "#">Privacy Policy</a>
-                                <a href = "#">Safety Center</a>
-                                <a href = "#">Security</a>
-                                <a href = "#">Terms</a>
+                                <a onClick={hideMore} href = "#">Accessibility Statement</a>
+                                <a onClick={hideMore} href = "#">Ad Choices</a>
+                                <a onClick={hideMore} href = "#">Community Guidelines</a>
+                                <a onClick={hideMore} href = "#">Cookie Policy</a>
+                                <a onClick={hideMore} href = "#">Help</a>
+                                <a onClick={hideMore} href = "#">Privacy Policy</a>
+                                <a onClick={hideMore} href = "#">Safety Center</a>
+                                <a onClick={hideMore} href = "#">Security</a>
+                                <a onClick={hideMore} href = "#">Terms</a>
                             </div>
                         </div>
-                        : null}
+                        }
                     </div>
                 </div>
                 <div className="center">
@@ -76,38 +118,46 @@ const Nav =() => {
                 </div>
                 <div className="right">
                     <div className="dropdown">
-                        <img onClick={primeClicked} className="loot" title = "prime loot" alt = "prime loot" src={require("../../icons/primeLoot.png")}></img>
-                        {primeToggled ?
-                        <div class="loot-dropdown">
+                        <img onClick={() => setPrimeToggled(!primeToggled)} ref={primeNode} className="loot" title = "prime loot" alt = "prime loot" src={require("../../icons/primeLoot.png")}></img>
+                        {primeToggled &&
+                        <div ref={primeText} class="loot-dropdown">
                             <p>Prime Gaming Loot</p>
                     
                         </div>
-                        : null }
+                        }
                     </div>
                     <button className="logIn">Log In</button>
                     <button className="signUp">Sign Up</button>
                     <div class="dropdown">
-                        <img onClick={profileClicked} className="profile" src={require("../../icons/profileIcon.png")} alt = "profile" />
-                        {profileToggled ?
-                        <div className="profile-dropdown">
+                        <img onClick={() => { setProfileToggled(!profileToggled) }} ref={profileNode} className="profile" src={require("../../icons/profileIcon.png")} alt = "profile" />
+                        {profileToggled &&
+                        <div ref={profileContent} className="profile-dropdown">
                             <div>
-                                <p><img src={require('../../icons/languageIcon.png')}></img> Language</p>                        
-                                <p style={{fontSize: "larger"}}>{`>`}</p>
+                                <div>
+                                    <img src={require('../../icons/languageIcon.png')}></img>
+                                    <p>Language</p>
+                                </div>                        
+                                <img src={require('../../icons/arrowIcon.png')} style={ {width: "15px", height: "15px"} }/>
                             </div>
                             <div>
-                                <p><img src={require('../../icons/themeIcon.png')}
-                                style = {{transform: "rotate(-45deg)"}}></img> Dark Theme</p>                        
-                                <p>{`<`}</p>
+                                <div>
+                                    <img src={require('../../icons/themeIcon.png')}
+                                    style = {{transform: "rotate(-45deg)"}}></img>
+                                    <p>Dark Theme</p>
+                                </div>                       
+                                
                             </div>
                             <div>
-                                <p><img src={require('../../icons/loginIcon.png')} /> Log In</p>                                  
+                                <div>
+                                    <img src={require('../../icons/loginIcon.png')} />
+                                    <p> Log In</p>
+                                </div>                                  
                             </div>
                         </div>
-                        : null}
+                        }
                     </div>
                 </div>
             </nav>
-        </body>
     )
 };
 
